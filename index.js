294f73e7665d43a27d8c64c9595206d48c0f90b0
ShaneWilmes <shanewilmes@yahoo.com>
2022-04-23 10:43:29 -0500

    questions working in cmd line

--- index.js
+++ index.js
@@ -3,50 +3,58 @@ const inquirer = require('inquirer');  // imports inquirer package or package-lo
 const fs = require('fs');  // imports file system
 
 // TODO: Create an array of questions for user input
-const generateREADME =
-
 inquirer
     .prompt([
         {
             type: 'input',
-            userName: 'userName',
+            name: 'userName',
             message: 'What is your GitHub username?',
         },
         {
             type: 'input',
-            email: 'email',
+            name: 'email',
             message: 'What is your email address?',
         },
         {
             type: 'input',
-            projectName: 'projectName',
+            name: 'projectName',
             message: 'What is the name of your project?',
 
         },
         {
             type: 'input',
-            description: 'description',
+            name: 'description',
             message: 'Give a brief description of your project',
         },
         {
             type: 'list',
-            message: 'What kind of license should your project have?',
-            licenseType: 'licenseType',
+            name: 'licenseType',
+            message: 'What kind of license should your project have?',            
             choices: ['ISC', 'MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
-
         },
-        // {
-        //     message: 'What command should be run to install dependencies?',
-        //     default: 'npm i',
-        // },
-
+        {
+            type: 'input',
+            name: 'npmi',
+            message: 'What command should be run to install dependencies?',
+            default: 'nmp test'
+        },
+        {
+            type: 'input',
+            name: 'usingRepo',
+            message: 'What does the user need to know about using the repo?',
+        },
+        
     ]);
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+
+    // // TODO: Create a function to write README file
+
+
+
 
 // // TODO: Create a function to initialize app
 // function init() {}
 
 // // Function call to initialize app
 // init();
+
